28bec7baecf41bd8d5cbc39f1160a1f8c2117e26
nido425 <nido425.tito@gmail.com>
2013-08-19 04:58:18 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Yonch::Application.config.secret_key_base = '0d0021b9e96263b9feb7258a7e674fbf8ecc3da9cf8d5147d7ac333651faf2bf4b0dc0271355a1c3b18857c17ea758a359f3b5c2c589b441f90b5cf28d5cce75'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Yonch::Application.config.session_store :cookie_store, key: '_yonch_session'
